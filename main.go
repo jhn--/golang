f23dd99f6ffb11bc760db96dbfc4b1355112c3c4
John <john.hoe@gmail.com>
2018-12-18 01:12:36 +0800

    Enter a temperature in Fahrenheit: 100 37.77777777777778

--- main.go
+++ main.go
@@ -2,13 +2,15 @@ package main
 
 import "fmt"
 
-// this is a comment 
+/*
+Using the example program as a starting point, write a program that converts from Fahrenheit into Celsius. (C = (F - 32) * 5/9)
+*/
 
 func main() {
-    fmt.Print("Enter a number: ")
-    var input float64
-    fmt.Scanf("%f", &input)
+    fmt.Print("Enter a temperature in Fahrenheit: ")
+    var fahrenheit float64
+    fmt.Scanf("%f", &fahrenheit)
 
-    output := input * 2
-    fmt.Println(output)
+    celsius := ((fahrenheit - 32.0) * (5.0/9.0))
+    fmt.Println(celsius)
 }
