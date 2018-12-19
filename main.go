fca8e4dc1f72c618fc591a8c485ac741ee498676
John <john.hoe@gmail.com>
2018-12-19 17:57:30 +0800

    $ go run main.go                                                                                                                                                                            [23:17:31] [1 2 3] [1 2 3 4 5]

--- main.go
+++ main.go
@@ -7,36 +7,7 @@ slices
 */
 
 func main() {
-    // x := [5]float64 {98, 93, 77, 82, 83}
-
-    /*
-    x := [5]float64 {
-        98,
-        93,
-        77,
-        82,
-        83,
-    }
-    */
-
-    var x [10]int
-    fmt.Println(x)
-
-    y := [10]int {1,2,3,4,5,6,7,8,9}
-    fmt.Println(y)
-
-    z := make([]float64, 5, 10)
-    fmt.Println(z)
-
-    a := y[:5]
-    fmt.Println(a)
-
-    b := y[3:8]
-    fmt.Println(b)
-
-    c := y[:3]
-    fmt.Println(c)
-
-    d := y[:]
-    fmt.Println(d)
+    slice1 := []int{1,2,3}
+    slice2 := append(slice1, 4, 5)
+    fmt.Println(slice1, slice2)
 }
