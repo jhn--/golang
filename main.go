db689a0067e9066fdfef780e1be62c4c65d1021f
John <john.hoe@gmail.com>
2018-12-20 01:06:24 +0800

    $ go run main.go                                                                                                                                                                             [1:06:00] Lithium solid map[name:Neon state:gas]
    map[C:map[name:Carbon state:solid] F:map[name:Fluorine state:gas] Ne:map[name:Neon state:gas] Be:map[state:solid name:Beryllium] B:map[name:Boron state:solid] Li:map[name:Lithium state:solid] N:map[name:Nitrogen state:gas] O:map[name:Oxygen state:gas] H:map[name:hydrogen state:gas] He:map[name:helium state:gas]]

--- main.go
+++ main.go
@@ -3,58 +3,58 @@ package main
 import "fmt"
 
 /*
-map 1
+nested map
 */
 
 func main() {
-    x := make(map[string]int)
-    x["one"] = 1
-    x["two"] = 2
-    x["three"] = 3
-    x["four"] = 4
-    x["five"] = 5
-
-    fmt.Println(x)
-    fmt.Println(x["one"])
-    fmt.Println("length of x is", len(x))
-
-    // delete element with key "one" from map x.
-    delete(x, "one")
-
-    fmt.Println(x)
-    fmt.Println("length of x is", len(x))
-
-    // element can be found, return value and true
-    name, ok := x["five"]
-    fmt.Println(name, ok)
-
-    // element cannot be found, return 0 and false
-    noname, no_ok := x["cannot_be_found"]
-    fmt.Println(noname, no_ok)
-
-    // if-else statement 
-    if name, ok := x["four"]; ok{
-        fmt.Println(name, ok)
-    }else{
-        fmt.Println("can't find.")
+    elements := map[string]map[string]string{
+        "H":map[string]string{
+            "name": "hydrogen",
+            "state": "gas",
+        },
+        "He":map[string]string{
+            "name": "helium",
+            "state": "gas",
+        },
+        "Li": map[string]string{
+          "name":"Lithium",
+          "state":"solid",
+        },
+        "Be": map[string]string{
+          "name":"Beryllium",
+          "state":"solid",
+        },
+        "B":  map[string]string{
+          "name":"Boron",
+          "state":"solid",
+        },
+        "C":  map[string]string{
+          "name":"Carbon",
+          "state":"solid",
+        },
+        "N":  map[string]string{
+          "name":"Nitrogen",
+          "state":"gas",
+        },
+        "O":  map[string]string{
+          "name":"Oxygen",
+          "state":"gas",
+        },
+        "F":  map[string]string{
+          "name":"Fluorine",
+          "state":"gas",
+        },
+        "Ne":  map[string]string{
+          "name":"Neon",
+          "state":"gas",
+        },
     }
 
-    y := make(map[int]int)
-    y[1] = 1
-    fmt.Println(y)
-    fmt.Println(y[1])
-
-    z := make(map[int]string)
-    z[1] = "one"
-    fmt.Println(z)
-    fmt.Println(z[1])
-
-    // shorter way of creating maps
-    t := map[string]string{
-        "a":"apple",
-        "b":"bus",
-        "c":"cat",
-        "d":"dog",
+    if el, ok := elements["Li"]; ok {
+        fmt.Println(el["name"], el["state"])
     }
-    fmt.Println(t)
+
+    fmt.Println(elements["Ne"])
+    fmt.Println()
+    fmt.Println(elements)
 }
