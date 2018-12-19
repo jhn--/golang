01f7ba357342925087cfb151d0436277009c5f23
John <john.hoe@gmail.com>
2018-12-20 00:28:33 +0800

    $ go run main.go                                                                                                                                                                             [0:27:10] map[three:3 four:4 five:5 one:1 two:2] 1 length of x is 5 map[two:2 three:3 four:4 five:5] length of x is 4 5 true 0 false 4 true map[1:1] 1 map[1:one] one map[a:apple b:bus c:cat d:dog]

--- main.go
+++ main.go
@@ -3,12 +3,58 @@ package main
 import "fmt"
 
 /*
-slices copy
+map 1
 */
 
 func main() {
-    slice1 := []int{1,2,3}
-    slice2 := make([]int, 2)
-    copy(slice2, slice1)
-    fmt.Println(slice1, slice2)
+    x := make(map[string]int)
+    x["one"] = 1
+    x["two"] = 2
+    x["three"] = 3
+    x["four"] = 4
+    x["five"] = 5
+
+    fmt.Println(x)
+    fmt.Println(x["one"])
+    fmt.Println("length of x is", len(x))
+
+    // delete element with key "one" from map x.
+    delete(x, "one")
+
+    fmt.Println(x)
+    fmt.Println("length of x is", len(x))
+
+    // element can be found, return value and true
+    name, ok := x["five"]
+    fmt.Println(name, ok)
+
+    // element cannot be found, return 0 and false
+    noname, no_ok := x["cannot_be_found"]
+    fmt.Println(noname, no_ok)
+
+    // if-else statement 
+    if name, ok := x["four"]; ok{
+        fmt.Println(name, ok)
+    }else{
+        fmt.Println("can't find.")
+    }
+
+    y := make(map[int]int)
+    y[1] = 1
+    fmt.Println(y)
+    fmt.Println(y[1])
+
+    z := make(map[int]string)
+    z[1] = "one"
+    fmt.Println(z)
+    fmt.Println(z[1])
+
+    // shorter way of creating maps
+    t := map[string]string{
+        "a":"apple",
+        "b":"bus",
+        "c":"cat",
+        "d":"dog",
+    }
+    fmt.Println(t)
 }
