146a2679a80df974f26c14ec98975bf1ab611713
John <john.hoe@gmail.com>
2018-12-20 12:53:42 +0800

    $ go run main.go                                                                                                                                                                            [12:52:58] smallest value is 9

--- main.go
+++ main.go
@@ -3,58 +3,34 @@ package main
 import "fmt"
 
 /*
-nested map
+Write a program that finds the smallest number in this list:
+
+x := []int{
+  48,96,86,68,
+  57,82,63,70,
+  37,34,83,27,
+  19,97, 9,17,
+}
 */
 
 func main() {
-    elements := map[string]map[string]string{
-        "H":map[string]string{
-            "name": "hydrogen",
-            "state": "gas",
-        },
-        "He":map[string]string{
-            "name": "helium",
-            "state": "gas",
-        },
-        "Li": map[string]string{
-          "name":"Lithium",
-          "state":"solid",
-        },
-        "Be": map[string]string{
-          "name":"Beryllium",
-          "state":"solid",
-        },
-        "B":  map[string]string{
-          "name":"Boron",
-          "state":"solid",
-        },
-        "C":  map[string]string{
-          "name":"Carbon",
-          "state":"solid",
-        },
-        "N":  map[string]string{
-          "name":"Nitrogen",
-          "state":"gas",
-        },
-        "O":  map[string]string{
-          "name":"Oxygen",
-          "state":"gas",
-        },
-        "F":  map[string]string{
-          "name":"Fluorine",
-          "state":"gas",
-        },
-        "Ne":  map[string]string{
-          "name":"Neon",
-          "state":"gas",
-        },
+    x := []int{
+        48,96,86,68,
+        57,82,63,70,
+        37,34,83,27,
+        19,97, 9,17,
     }
 
-    if el, ok := elements["Li"]; ok {
-        fmt.Println(el["name"], el["state"])
-    }
+    smallest := x[0]
 
-    fmt.Println(elements["Ne"])
-    fmt.Println()
-    fmt.Println(elements)
+    for i := 1; i < len(x); i++ {
+        // fmt.Println(x[i])
+        if x[i] < smallest{
+            // smallest := x[i]
+            // fmt.Println(x[i], "is less than", smallest)
+            // fmt.Println(x[i], "will be the smallest value.")
+            smallest = x[i]
+        }
+    }
+    fmt.Println("smallest value is", smallest)
 }
