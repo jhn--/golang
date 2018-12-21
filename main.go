13433d893df3e1581e6350b0bad954efc1a99d64
John <john.hoe@gmail.com>
2018-12-22 01:53:21 +0800

    Ark :: projects/programming/golang ‹master*› % go run main.go 86.6

--- main.go
+++ main.go
@@ -3,34 +3,24 @@ package main
 import "fmt"
 
 /*
-Write a program that finds the smallest number in this list:
-
-x := []int{
-  48,96,86,68,
-  57,82,63,70,
-  37,34,83,27,
-  19,97, 9,17,
-}
+functions
 */
 
 func main() {
-    x := []int{
-        48,96,86,68,
-        57,82,63,70,
-        37,34,83,27,
-        19,97, 9,17,
-    }
+    xs := []float64{98,93,77,82,83}
+    fmt.Println(average(xs))
+}
 
-    smallest := x[0]
 
-    for i := 1; i < len(x); i++ {
-        // fmt.Println(x[i])
-        if x[i] < smallest{
-            // smallest := x[i]
-            // fmt.Println(x[i], "is less than", smallest)
-            // fmt.Println(x[i], "will be the smallest value.")
-            smallest = x[i]
-        }
+func average(xs []float64) float64 {
+    total := 0.0
+    for _, value := range xs {
+        total += value
     }
-    fmt.Println("smallest value is", smallest)
+    /*
+    for i := 0; i < len(xs); i++ {
+        total += xs[i]
+    }
+    */
+    return total/float64(len(xs))
 }
