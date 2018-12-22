d0f54025d423971e76001dc8af3f1aed6261d57a
John <john.hoe@gmail.com>
2018-12-23 03:35:53 +0800

    Ark:golang (master*) $ go run main.go 100

--- main.go
+++ main.go
@@ -5,33 +5,25 @@ import "fmt"
 /*
 functions
 
-Write a function which takes an integer and halves it and returns true if it was even or false if it was odd. For example half(1) should return (0, false) and half(2) should return (1, true).
+Write a function with one variadic parameter that finds the greatest number in a list of numbers.
 */
 
-func gethalf(arg int) (int, bool) {
-    fmt.Println(arg)
-    div := arg / 2
-    if arg % 2 == 0 {
-        return div, true
+func getLargest(args ...int) int {
+    if len(args) == 1 {
+        return args[0]
     } else {
-        return div, false
+        largest := args[0]
+        for _, value := range args {
+            if value > largest {
+                largest = value
+            }
+        }
+        return largest
     }
 }
 
 
 func main() {
-    a, b := gethalf(1)
-    fmt.Println(a, b)
-    c, d := gethalf(2)
-    fmt.Println(c, d)
-    e, f := gethalf(3)
-    fmt.Println(e, f)
-    g, h := gethalf(4)
-    fmt.Println(g, h)
-    i, j := gethalf(5)
-    fmt.Println(i, j)
-    k, l := gethalf(6)
-    fmt.Println(k, l)
-    m, n := gethalf(7)
-    fmt.Println(m, n)
+    a := getLargest(3,100,5,7,8,5,3,23,6,8,5,3)
+    fmt.Println(a)
 }
