3ddca325990e19b34bf691bf7aabcc95e0bfd895
John <john.hoe@gmail.com>
2018-12-23 03:41:44 +0800

    Ark:golang (master*) $ go run main.go 1 3 5 7

--- main.go
+++ main.go
@@ -5,25 +5,27 @@ import "fmt"
 /*
 functions
 
-Write a function with one variadic parameter that finds the greatest number in a list of numbers.
+Using makeEvenGenerator as an example, write a makeOddGenerator function that generates odd numbers.
 */
 
-func getLargest(args ...int) int {
-    if len(args) == 1 {
-        return args[0]
-    } else {
-        largest := args[0]
-        for _, value := range args {
-            if value > largest {
-                largest = value
-            }
+func makeOddGenerator() func() uint {
+    i := uint(0)
+    return func() (ret uint) {
+        if i == 0 {
+            i++
+            ret = i
+            return
         }
-        return largest
+        i += 2
+        ret = i
+        return
     }
 }
 
-
 func main() {
-    a := getLargest(3,100,5,7,8,5,3,23,6,8,5,3)
-    fmt.Println(a)
+    makeOdd := makeOddGenerator()
+    fmt.Println(makeOdd())
+    fmt.Println(makeOdd())
+    fmt.Println(makeOdd())
+    fmt.Println(makeOdd())
 }
