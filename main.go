378fbc8a5dd9cd3a9a15ed40de1264cf122c37bb
John <john.hoe@gmail.com>
2018-12-22 18:56:37 +0800

    ➜  golang git:(master) ✗ go run main.go 1 2 3 4

--- main.go
+++ main.go
@@ -6,13 +6,20 @@ import "fmt"
 functions
 
 Closure
+increment() adds 1 to the variable x which is defined in the main function's scope. This x variable _can_ be accessed and modified by the increment function. This is why the first time we call increment we see 1 displayed, but the second time we call it we see 2 displayed.
+
+A function like this together with the non-local variables it references is known as a closure. In this case increment and the variable x form the closure.
 */
 
 func main() {
-    add := func(x, y int) int {
-        return x + y
+    x := 0
+    increment := func() int {
+        x++
+        return x
     }
 
-    fmt.Println("Closure")
-    fmt.Println(add(1,2))
+    fmt.Println(increment())
+    fmt.Println(increment())
+    fmt.Println(increment())
+    fmt.Println(increment())
 }
