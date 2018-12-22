4ec25dabc5962d56f64e86092031a3519cc1be67
John <john.hoe@gmail.com>
2018-12-22 18:42:09 +0800

    ➜  golang git:(master) ✗ go run main.go Closure 3

--- main.go
+++ main.go
@@ -5,30 +5,14 @@ import "fmt"
 /*
 functions
 
-Variadic Functions
+Closure
 */
 
 func main() {
-    xs := []int{1,2,3,4,5,6}
-    total_1 := add(xs...)
-    fmt.Println("total_1 from array = ",total_1)
-
-    xxs := xs[:3]
-    total_slice := add(xxs...)
-    fmt.Println("xxs = ",xxs)
-    fmt.Println("total_slice from slice \"xxs\" = ",total_slice)
-
-    total_2 := add(10, 9, 8)
-    fmt.Println("total_2 from a bunch of ints = ",total_2)
-
-    total_3 := add()
-    fmt.Println("total_3 from zero arguments =",total_3)
-}
-
-func add(args ...int) (total int) {
-    // accepts 0 or more arguments
-    for _, value := range args {
-        total += value
+    add := func(x, y int) int {
+        return x + y
     }
-    return 
+
+    fmt.Println("Closure")
+    fmt.Println(add(1,2))
 }
