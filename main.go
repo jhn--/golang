056d74f53a595eaae9ce5196205f11e00ce41a69
John <john.hoe@gmail.com>
2018-12-22 18:39:42 +0800

    ➜  golang git:(master) go run main.go                                                                                                                                                       2 ↵ total_1 from array =  21 xxs =  [1 2 3] total_slice from slice "xxs" =  6 total_2 from a bunch of ints =  27 total_3 from zero arguments = 0

--- main.go
+++ main.go
@@ -11,7 +11,12 @@ Variadic Functions
 func main() {
     xs := []int{1,2,3,4,5,6}
     total_1 := add(xs...)
-    fmt.Println("total_1 from array/slice = ",total_1)
+    fmt.Println("total_1 from array = ",total_1)
+
+    xxs := xs[:3]
+    total_slice := add(xxs...)
+    fmt.Println("xxs = ",xxs)
+    fmt.Println("total_slice from slice \"xxs\" = ",total_slice)
 
     total_2 := add(10, 9, 8)
     fmt.Println("total_2 from a bunch of ints = ",total_2)
