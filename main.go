f296db466792ba5980d4f1e6384ac5d0cc6a908c
John <john.hoe@gmail.com>
2018-12-23 03:27:33 +0800

    Ark:golang (master*) $ go run main.go 1 0 false 2 1 true 3 1 false 4 2 true 5 2 false 6 3 true 7 3 false

--- main.go
+++ main.go
@@ -5,18 +5,33 @@ import "fmt"
 /*
 functions
 
-defer
+Write a function which takes an integer and halves it and returns true if it was even or false if it was odd. For example half(1) should return (0, false) and half(2) should return (1, true).
 */
 
-func first() {
-    fmt.Println("first")
+func gethalf(arg int) (int, bool) {
+    fmt.Println(arg)
+    div := arg / 2
+    if arg % 2 == 0 {
+        return div, true
+    } else {
+        return div, false
+    }
 }
 
-func second() {
-    fmt.Println("second")
-}
 
 func main() {
-    defer first()
-    second()
+    a, b := gethalf(1)
+    fmt.Println(a, b)
+    c, d := gethalf(2)
+    fmt.Println(c, d)
+    e, f := gethalf(3)
+    fmt.Println(e, f)
+    g, h := gethalf(4)
+    fmt.Println(g, h)
+    i, j := gethalf(5)
+    fmt.Println(i, j)
+    k, l := gethalf(6)
+    fmt.Println(k, l)
+    m, n := gethalf(7)
+    fmt.Println(m, n)
 }
