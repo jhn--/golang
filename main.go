53081b8b5ca5aaffb9fa4fef94fe32af7e8f2d98
John <john.hoe@gmail.com>
2018-12-23 14:51:36 +0800

    Ark:golang (master*) $ go run main.go fib for 0  is: 0 fib for 1  is: 1 fib for 2  is: 1 fib for 3  is: 2 fib for 4  is: 3 fib for 5  is: 5 fib for 6  is: 8 fib for 7  is: 13 fib for 8  is: 21 fib for 9  is: 34 fib for 10  is: 55 fib for 11  is: 89 fib for 12  is: 144 fib for 13  is: 233 fib for 14  is: 377 fib for 15  is: 610

--- main.go
+++ main.go
@@ -5,27 +5,22 @@ import "fmt"
 /*
 functions
 
-Using makeEvenGenerator as an example, write a makeOddGenerator function that generates odd numbers.
+The Fibonacci sequence is defined as: fib(0) = 0, fib(1) = 1, fib(n) = fib(n-1) + fib(n-2). Write a recursive function which can find fib(n).
 */
 
-func makeOddGenerator() func() uint {
-    i := uint(0)
-    return func() (ret uint) {
-        if i == 0 {
-            i++
-            ret = i
-            return
-        }
-        i += 2
-        ret = i
-        return
+func fibonacci(x uint) uint {
+    if (x == 0) {
+        return 0
+    } else if (x == 1) {
+        return 1
+    } else {
+        return (fibonacci(x - 1) + fibonacci(x - 2))
     }
 }
 
 func main() {
-    makeOdd := makeOddGenerator()
-    fmt.Println(makeOdd())
-    fmt.Println(makeOdd())
-    fmt.Println(makeOdd())
-    fmt.Println(makeOdd())
+    xs := []uint{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}
+    for _, value := range xs {
+        fmt.Println("fib for", value, " is:", fibonacci(value))
+    }
 }
