c094acbec7f6b396ec03df2ded7070570fbdec53
John <john.hoe@gmail.com>
2018-12-24 15:58:42 +0800

    Ark:golang (master*) $ go run main.go x is  5 with address of 0xc00008a000 x is now  0 with address of  0xc00008a000
    even without using pointers I'm able to change the value for the same variable (based on memory address)

--- main.go
+++ main.go
@@ -6,14 +6,14 @@ import "fmt"
 pointers
 */
 
-func zero(x int) {
+func zero(x int) int {
     x = 0
+    return x
 }
 
 func main() {
-    // zero function will not modify the original x variable in the main function
     x := 5
-    fmt.Println("x is ", x)
-    zero(x)
-    fmt.Println("after zero(), x is now", x)
+    fmt.Println("x is ",x ,"with address of", &x)
+    x = zero(x) // why &x ?
+    fmt.Println("x is now ",x ,"with address of ", &x)
 }
