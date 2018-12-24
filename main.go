589268bded9e6fa0abefe2059b86927f685ad9f8
John <john.hoe@gmail.com>
2018-12-25 02:43:24 +0800

    john@Ark programming/golang (master *%) » go run main.go 10 10 5 78.53981633974483

--- main.go
+++ main.go
@@ -2,18 +2,24 @@ package main
 
 import (
     "fmt";
+    "math"
 )
 
 /*
 Structs and Interfaces
 */
 
-func main() {
-    type Circle struct {
-        x, y, r float64
-    }
+type Circle struct {
+    x, y, r float64
+}
 
+func (c *Circle) area() float64 {
+    return math.Pi * c.r * c.r
+}
+
+func main() {
     c := Circle{x: 10, y: 10, r: 5}
 
     fmt.Println(c.x, c.y, c.r)
+    fmt.Println(c.area())
 }
