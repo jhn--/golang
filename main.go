56259c04d530b873866dd37c453c403f95fceff0
John <john.hoe@gmail.com>
2018-12-24 16:02:50 +0800

    Ark:golang (master*) $ go run main.go x is  5 with address of 0xc000092000 x is now  0 with address of  0xc000092000
    with pointer there's no need to return value, it seems we're directly editing the value in the address.

--- main.go
+++ main.go
@@ -6,14 +6,15 @@ import "fmt"
 pointers
 */
 
-func zero(x int) int {
-    x = 0
-    return x
+func zero(xPtr *int) { // whats * for?
+    *xPtr = 0
 }
 
 func main() {
     x := 5
     fmt.Println("x is ",x ,"with address of", &x)
-    x = zero(x) // why &x ?
+    zero(&x) // why &x ?
     fmt.Println("x is now ",x ,"with address of ", &x)
 }
+
+// * and & seems to be syntax for pointers etc, find out more.
