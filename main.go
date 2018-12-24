b53fdbd5c527cfd42b4437d9f5c6e1b7dd07b2a7
John <john.hoe@gmail.com>
2018-12-24 18:59:18 +0800

    % go run main.go                                                                                                                                                                                  ✹ ✭ 1.5 2.25

--- main.go
+++ main.go
@@ -6,17 +6,13 @@ import "fmt"
 pointers
 */
 
+func square(x *float64) {
+    fmt.Println(*x)
+    *x = *x * *x
+}
+
 func main() {
-    one := "1: How do you get the memory address of a variable?"
-    one_ans := "By using the & operator."
-    two := "2: How do you assign a value to a pointer?"
-    two_ans := "using the * operation, ie *xPtr = 0, means 'store the value 0 in the memory address xPtr refers to.'"   
-    three := "3: How do you create a new pointer?"
-    three_ans := "by using the new() function, new takes a type as an argument ie. new(int), and returns a pointer."
-
-    qns_ans := []string {one, one_ans, two, two_ans, three, three_ans}
-
-    for _, v := range qns_ans {
-        fmt.Println(v)
-    }
+    x := 1.5
+    square(&x)
+    fmt.Println(x)
 }
