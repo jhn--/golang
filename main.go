124ba2606f00b3df1719aaeceddf884583cdd6bd
John <john.hoe@gmail.com>
2018-12-25 01:07:15 +0800

    john@Ark programming/golang (master *%) » go run main.go original x =  1 original y =  2 new x =  2 new y =  1

--- main.go
+++ main.go
@@ -4,15 +4,29 @@ import "fmt"
 
 /*
 pointers
+
+Write a program that can swap two integers (x := 1; y := 2; swap(&x, &y) should give you x=2 and y=1).
 */
 
-func square(x *float64) {
-    fmt.Println(*x)
-    *x = *x * *x
+func swap(xPtr *int, yPtr *int) {
+    zPtr := new(int)
+    *zPtr = *xPtr
+
+    *xPtr = *yPtr
+    *yPtr = *zPtr
 }
 
 func main() {
-    x := 1.5
-    square(&x)
-    fmt.Println(x)
+    x := 1
+    y := 2
+    fmt.Println("original x = ", x)
+    fmt.Println("original y = ", y)
+    swap(&x, &y)
+    fmt.Println("new x = ", x)
+    fmt.Println("new y = ", y)
 }
+
+// create a buffer pointer(zPtr)
+// assign value of xPtr to zPtr
+// assign value of yPtr to xPtr
+// assign value of zPtr to yPtr
